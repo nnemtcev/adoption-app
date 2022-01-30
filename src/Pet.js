6aafce1bb2c08a94220ec125d5a12542abec31c2
Nick Nemtcev <nicknemtcev@outlook.com>
2022-01-30 06:09:30 -0500

    Install and configure Babel and refactor Pet to use JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,14 @@
-import React from "react";
-
 // You can pass props into a component like so.
 // Also, you could destructure the props in the parameters section of the function.
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-}
+const Pet = ({ name, animal, breed }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
