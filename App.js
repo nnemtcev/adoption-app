9417cf96d52c6596a0b143ed9ec67a106c120447
Nick Nemtcev <nicknemtcev@outlook.com>
2022-01-30 05:10:14 -0500

    Refactor Pet component to accept props as an argument

---
+++ App.js
@@ -0,0 +1,41 @@
+// You can pass props into a component like so.
+// Also, you could destructure the props in the parameters section of the function.
+
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
+// To pass multiple children into a component
+// you can either pass it multiple elements in an array,
+// or you can pass it multiple arguments (each argument being a separate child).
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Havanese",
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "Bird",
+                breed: "Cockatiel",
+            }),
+            React.createElement(Pet, {
+                name: "Doink",
+                animal: "Cat",
+                breed: "Mix",
+            }),
+        ]
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
